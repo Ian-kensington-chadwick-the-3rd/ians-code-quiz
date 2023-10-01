3483a74ae2c62dc9547f286d533f81703300a75c
Ian Sills <iansills04@gmail.com>
2023-10-01 13:29:36 -0500

    updated html/css/js

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,69 @@
-var button = document.querySelector("#btn");
-var timerEl = document.getElementById('countdown');
-var questionArray = document.getElementById('question');
-const startButton = document.getElementById('start-btn');
-startButton.addEventListener('click', startGame);
+// var button = document.querySelector("#btn")
+// var timerEl = document.getElementById('countdown')
+var questionArray = document.getElementById('question')
+const startButton = document.getElementById('start-btn')
+startButton.addEventListener('click', startGame)
+nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+let shuffledQuestions, currentQuestionIndex
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+function startGame() {
+console.log('started')
+startButton.classList.add('hide')
+shuffledQuestions = questions.sort(() => Math.random()- .5)
+currentQuestionIndex = 0
+questionContainerElement.classList.remove('hide')
+setNextQuestion()
+
+}
+
+function setNextQuestion(){
+   resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question){
+questionElement.innerText = question.question
+question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if(answer.correct){
+        button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState(){
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild){
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
 
 
-var trueArr = [""]
-var highscore = [""]
+function selectAnswer(e) {
+
+
+
+}
+
+const questions =[
+{
+    question: "What is 2 +2",
+    answers:[
+     {text: '4', correct: true },   
+     {text: '22', correct: false},
+    ]
+}
+
+
+]
 
 
 // first I need to have an array for answer
@@ -16,6 +73,9 @@ var highscore = [""]
 // the time will be decreased every time I get an answer incorrect
 // I need too add an event listener every time they click an answer
 // I need too add an persistant storage for a total score when the person is finish it will the go under highscore
+
+
+
 
 // button.addEventListener("click", function() {countdown()});
 
@@ -35,24 +95,3 @@ var highscore = [""]
 //       }
 //     }, 1000);
 //   }
-  
-function startGame() {
-console.log('started')
-
-}
-
-function setNextQuestion(){
-
-}
-
-function selectanswer() {
-
-
-
-}
-
-
-
-
-
-
