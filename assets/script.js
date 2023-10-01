4ad8cad3a5b11fc377ee9d3060d9699518880157
Ian Sills <iansills04@gmail.com>
2023-09-30 21:30:50 -0500

    modified html/css/js

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,24 @@
 var button = document.querySelector("#btn")
 var timerEl = document.getElementById('countdown');
+var questionArray = document.getElementById('question')
+startButton.addEventListener("click", startGame)
 
 
-button.addEventListener("click", function() {
-countdown()
-});
+var trueArr = [""]
+var highscore = [""]
+
+
+// first I need to have an array for answer
+// Ill make two answer arrays true of false
+// second I need the questions
+// the questions will be an if esle format
+// the time will be decreased every time I get an answer incorrect
+// I need too add an event listener every time they click an answer
+// I need too add an persistant storage for a total score when the person is finish it will the go under highscore
+// 
+button.addEventListener("click", function() {countdown()});
+
+
 function countdown() {
     var timeLeft = 60;
     var timeInterval = setInterval(function () {
@@ -21,3 +35,22 @@ function countdown() {
     }, 1000);
   }
   
+function startGame() {
+console.log('started');
+}
+
+function setNextQuestion(){
+
+}
+
+function selectanswer() {
+
+
+
+}
+
+
+
+
+
+
