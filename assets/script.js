3b41e46ebee09a2bc3ff7f2b104f3e1256080d0f
Ian Sills <iansills04@gmail.com>
2023-09-30 22:25:14 -0500

    updated js

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
-var button = document.querySelector("#btn")
+var button = document.querySelector("#btn");
 var timerEl = document.getElementById('countdown');
-var questionArray = document.getElementById('question')
-startButton.addEventListener("click", startGame)
+var questionArray = document.getElementById('question');
+const startButton = document.getElementById('start-btn');
+startButton.addEventListener('click', startGame);
 
 
 var trueArr = [""]
@@ -15,28 +16,29 @@ var highscore = [""]
 // the time will be decreased every time I get an answer incorrect
 // I need too add an event listener every time they click an answer
 // I need too add an persistant storage for a total score when the person is finish it will the go under highscore
-// 
-button.addEventListener("click", function() {countdown()});
+
+// button.addEventListener("click", function() {countdown()});
 
 
-function countdown() {
-    var timeLeft = 60;
-    var timeInterval = setInterval(function () {
-      if (timeLeft > 1) {
-        timerEl.textContent = timeLeft;
-        timeLeft--;
-      } else if (timeLeft === 1) {
-        timerEl.textContent = timeLeft;
-        timeLeft--;
-      } else {
-        timerEl.textContent = '';
-        clearInterval(timeInterval);
-      }
-    }, 1000);
-  }
+// function countdown() {
+//     var timeLeft = 60;
+//     var timeInterval = setInterval(function () {
+//       if (timeLeft > 1) {
+//         timerEl.textContent = timeLeft;
+//         timeLeft--;
+//       } else if (timeLeft === 1) {
+//         timerEl.textContent = timeLeft;
+//         timeLeft--;
+//       } else {
+//         timerEl.textContent = '';
+//         clearInterval(timeInterval);
+//       }
+//     }, 1000);
+//   }
   
 function startGame() {
-console.log('started');
+console.log('started')
+
 }
 
 function setNextQuestion(){
