09ea202eff2239a0a7c28f697210d160ff79cc6b
Ian Sills <iansills04@gmail.com>
2023-10-02 19:22:39 -0500

    updated js

--- assets/script.js
+++ assets/script.js
@@ -38,7 +38,79 @@ startBtn.onclick = () => {
   showQuestions(queCount)
 };
 
+//Get Questions and Options from Array
+function showQuestions(index){
+  if (queCount>=10){
+      return;
+  }
+  const queText = document.querySelector(".que-text");
+  const optionList = document.querySelector("#choices");
+  let queTag = "<span>"+ questions[index].numb + ". "+ questions[index].question +"</span>";
+  let optionTag = '<div class="option">'+ questions[index].options[0] + '<span></span></div>'
+                  + '<div class="option">'+ questions[index].options[1] + '<span></span></div>'
+                  + '<div class="option">'+ questions[index].options[2] + '<span></span></div>'
+                  + '<div class="option">'+ questions[index].options[3] + '<span></span></div>'
+  queText.innerHTML = queTag;
+  optionList.innerHTML = optionTag;
+  const option = optionList.querySelectorAll(".option");
+  for (let i = 0; i < option.length; i++) {
+      option[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
 
+//Show Next Question When Question is answered
+function optionSelected(answer){
+  if (queCount>=10){
+      return;
+  }
+  let userAns = answer.textContent;
+  let correctAns = questions[queCount].answer;
+  if(userAns == correctAns){
+      console.log("Answer is Correct");
+      const response = document.querySelector("#response");
+      response.innerHTML = '<div id="response"><span>Correct!</span></div>';
+      setTimeout(nextQuestion, 500)
+      score += 1
+
+  }else{
+      console.log("Answer is Wrong");
+      const response = document.querySelector("#response");
+      response.innerHTML = '<div id="response"><span>Wrong!</span></div>';
+      setTimeout(nextQuestion, 500)
+      counter -= 5
+  }
+}
+function nextQuestion(){
+  queCount++;
+  if(queCount == 10){
+      
+      quizEnd()
+  };
+  showQuestions(queCount);
+  const response = document.querySelector("#response");
+  response.innerHTML = '<div id="response"><span></span></div>';
+  }
+
+//End Quiz if All Questions Completed or Timer Runs out
+function quizEnd(){
+  quizBox.classList.add("hide");
+  endBox.classList.remove("hide");
+  const scoreText = document.querySelector(".score");
+  let scoreTag = '<h3 class="score"> Your score was '+ score +' out of 10!</h3>';
+  scoreText.innerHTML = scoreTag; 
+}
+//Submit Initials
+submitBtn.onclick = () => {
+  let initials = initialsText.value;
+  //Store Initials and Score in Local Storage
+  var resultsDataObj = {
+      initials: initials,
+      score: score
+  }
+  localStorage.setItem((localStorage.length+1), JSON.stringify(resultsDataObj));
+  initialsText.value = ""
+  location.reload();
+}
 
 
 
